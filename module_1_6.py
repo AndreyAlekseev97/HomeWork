a459672794fb78bbf3b6900f4eeaabf8549384b0
Андрей <andrey.ustas1@gmail.com>
2024-11-19 20:48:20 +0500

    19/11 HW 2

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+quantity_of_homework = 12
+print (quantity_of_homework)
+hours_spent = 1.5
+print (hours_spent)
+cours_name = "Python"
+print(cours_name)
+time_for_tusk = hours_spent/quantity_of_homework
+print(time_for_tusk)
+print('Курс:', cours_name, 'всего задач:', quantity_of_homework, 'затрачено часов:', hours_spent, 'среднее время выолнения', time_for_tusk, 'часа')

0b1dee8758a6b8ffa235e97235aef1ca352ee900
Андрей <andrey.ustas1@gmail.com>
2024-11-21 23:20:58 +0500

    21/11 HW 4

---
+++ strings_2.py
@@ -0,0 +1,8 @@
+my_string = input ("Ваша строка с произвольным текстом: ")
+len_string = len(my_string)
+print("Длинна введеной строки: ", len_string)
+print ("Строка в верхнем регистре: ", my_string. upper())
+print ("Строка в нижнем регистре: ", my_string. lower())
+print("Строка без пробелов: ", my_string. replace(" ", ""))
+print(my_string [0])
+print(my_string [-1])
